09ae54dccebdd7a3ee2069c6640627c639497f1d
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-18 21:47:28 +0000

    initial setup

---
+++ lib/kh_cli_project.rb
@@ -0,0 +1,6 @@
+require "kh_cli_project/version"
+
+module KhCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kh_cli_project/version.rb
@@ -0,0 +1,3 @@
+module KhCliProject
+  VERSION = "0.1.0"
+end
