e4c98b765d49d759a86ecf8662a4705763a27edc
kam3r0nz <kam.hungerford@gmail.com>
2020-08-18 17:39:55 -0500

    edit cli

--- lib/kh_cli_project/cli.rb
+++ lib/kh_cli_project/cli.rb
@@ -1,6 +1,19 @@
 class KhCliProject::Cli
     def greeting
         puts "Hi! Welcome to the KH CLI Project!"
+        puts "Please choose recipes or exit:"
+        input = gets.chomp
+        if input == "recipes"
+            list_recipes
+        elsif input == "exit"
+            puts "Goodbye!"
+        end
+    end
+
+    def list_recipes
+        puts "1. Pan seared pork chops"
+        puts "2. Orange chicken"
+        puts "3. Mediterranean pizza"
     end
 
 end
